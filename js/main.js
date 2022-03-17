8b1fab972f4bb11a463f3c3fc4f50b3228bce835
simone andreozzi <simoneandreozzi8@gmail.com>
2022-03-17 15:48:10 +0100

    inserimento-click-attivazioni-immagini

---
+++ js/main.js
@@ -0,0 +1,59 @@
+const app = new Vue(
+    {
+        el: "#app",
+
+        data:{
+            attivazioneSlide: 0,            // è un numero che ci sta a indicare a che indice stiamo per mostrare sia l'immagine principale che la scroll in basso
+            lago :[
+                {
+                    url: 'https://siviaggia.it/wp-content/uploads/sites/2/2020/08/lago-di-ledro-perla-del-trentino-.jpg',
+                },
+                {
+                    url: 'https://cdn.marcopolo.tv/960x480/media/post/4e85frg/lago_di_scanno.jpg',
+                },
+                {
+                
+                    url: 'https://siviaggia.it/wp-content/uploads/sites/2/2020/04/lago-molveno-.jpg',
+                },
+                {
+                    url: 'https://www.guidatorino.com/wp-content/uploads/2020/08/lago-orfu-gad.jpg',
+                },
+                {
+                    url: 'https://www.toscana.info/wp-content/uploads/sites/123/lago-orecchiella-garfagnana-hd.jpg',
+                },
+                {
+                    url: 'https://www.bimbiviaggiatori.it/wp-content/uploads/2019/06/Lago-di-Ledro-passeggiate.jpg',
+                },
+            ]
+            
+        },
+        methods: {
+            cliccaImmagine(indexImg){
+                this.attivazioneSlide = indexImg
+            },
+
+            previous(){                                                 // per andare indietro con le immagine
+                if( this.attivazioneSlide === 0){
+                    this.attivazioneSlide = this.lago.length -1 // per tornare indietro
+                } else{
+                    this.attivazioneSlide --;
+                }
+            }, 
+
+            next(){                                                        // per andare avanti con le immagini
+                if( this.attivazioneSlide === this.lago.length -1 ){ // se sono all'ultimo elemento 
+                    this.attivazioneSlide = 0  //lo azzero 
+                } else{
+                    this.attivazioneSlide ++;
+                }
+            },
+
+            
+        }
+        
+    }
+)
+
+
+
+
